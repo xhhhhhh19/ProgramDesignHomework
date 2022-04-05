f44975d5bea6f818fa3ab10be62e87314aa79ba1
xhhhhhh19 <74998991+xhhhhhh19@users.noreply.github.com>
2022-04-05 13:11:11 +0800

    Update main.py
    按照portalToSQL类新的定义，更改了main.py文件；

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import portalToSQL as pts
 
-loginUrl = 'http://my.bupt.edu.cn/xs_index.jsp?urltype=tree.TreeTempUrl&wbtreeid=1541' # 登录页面的URL  试试看 原本的失效了
+loginUrl = 'http://my.bupt.edu.cn/xs_index.jsp?urltype=tree.TreeTempUrl&wbtreeid=1541' # 登录页面的URL
 # 由于北邮信息门户的特殊性，此处使用的是人工登录门户后获得的cookie
-cookie = {'cookie':'JSESSIONID=7BED8F109245DB55BF1F921C59C946E4'}
-notificationUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1154' # 校内通知的URL
+
+cookie = {'cookie':'JSESSIONID=D3DAE108CD370BFC4A6B768D9F403065'}
+
+#notificationUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1154' # 校内通知的URL
 notificationHref = 'xntz_content.jsp' # 校内通知的 href 标识
-announcementUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1305' # 公示公告的URL
+#announcementUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1305' # 公示公告的URL
 announcementHref = 'content.jsp'
-newsUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1221' # 校内新闻的URL
+#newsUrl = 'http://my.bupt.edu.cn/list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1221' # 校内新闻的URL
 newsHref = 'xnxw_content.jsp'
-lectureUrl = 'http://my.bupt.edu.cn/jz_list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1300' # 学术讲座的URL
+#lectureUrl = 'http://my.bupt.edu.cn/jz_list.jsp?urltype=tree.TreeTempUrl&wbtreeid=1300' # 学术讲座的URL
 lectureHref = 'hy_content.jsp'
 
     
 
 if __name__ == '__main__':
     
-    notification = pts.portalToSQL(loginUrl, cookie, notificationUrl, notificationHref, 'schoolArticles')
-    notification.getInformation()
-    
-    announcement = pts.portalToSQL(loginUrl, cookie, announcementUrl, announcementHref, 'schoolAnnouncements')
-    announcement.getInformation()
-    
-    
-    news = pts.portalToSQL(loginUrl, cookie, newsUrl, newsHref, 'schoolNews')
-    news.getInformation()
+    notification = pts.portalToSQL(loginUrl, cookie,notificationHref, 'schoolArticles', 120, 1154)
+    # df1 = notification.getInformation()
 
-    lecture = pts.portalToSQL(loginUrl, cookie, lectureUrl, lectureHref, 'schoolLectures')
-    lecture.getInformation()
+    announcement = pts.portalToSQL(loginUrl, cookie,announcementHref, 'schoolAnnouncements', 50, 1305)
+    # df2 = announcement.getInformation()
 
+    news = pts.portalToSQL(loginUrl, cookie,  newsHref, 'schoolNews', 165, 1221)
+    # df3 = news.getInformation()
+
+    lecture = pts.portalToSQL(loginUrl, cookie, lectureHref, 'schoolLectures', 5, 1300)
+    # df4 = lecture.getInformation()
+    '''
+    df1.to_csv("schoolArticles.csv")  # 公示公告
+    df2.to_csv("schoolAnnouncements.csv")  # 校内通知
+    df3.to_csv("schoolNews.csv")  # 校内新闻
+    df4.to_csv("schoolLectures.csv")  # 学术讲座
+    '''
     
